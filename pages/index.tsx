318f671b8595b26b1e8cdfdcbb1f7d332c6df086
Vietanh <vietanhcao1994@gmail.com>
2021-11-07 22:15:53 +0700

    test deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.1js!</a>
+          Welcome to <a href="https://nextjs.org">Next.1js -test!</a>
         </h1>
 
         <p className={styles.description}>
